11424e28535a7794032542170f521fea54e87180
Dmitriy Germanov <seltis.gtn@gmail.com>
2023-09-14 19:21:53 +0300

    added third task

---
+++ task02.py
@@ -0,0 +1,11 @@
+# Требуется вывести все целые степени двойки 
+# (т.е. числа вида 2k), не превосходящие числа N.
+
+N = int(input("Введите число N: "))
+
+degree_result = 1
+print(f"Степени двойки до N: {degree_result}", end = " ")
+degree_result = 2
+while (degree_result <= N):
+    print(degree_result, end = " ")
+    degree_result = degree_result * 2
